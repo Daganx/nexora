6b3de14ae63e23e8c02f238c653d54d7c8979599
Daganx <dagan.letot@icloud.com>
2026-06-26 17:00:18 +0200

    Update : Vercel HomePage

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
 import Hero from "@/components/home/Hero";
 import LatestWallpapers from "@/components/home/LatestWallpapers";
 
+export const dynamic = "force-dynamic";
+
 export const metadata = {
   title: "Nexora - Home",
 };
